56821d48a3f956b9bbc177fd88631eb4f62f85fe
Kishor Subedi <Kishor@Sudarshans-MacBook-Pro.local>
2018-03-03 12:33:04 -0500

    Chat saved

---
+++ ChatViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ChatViewController.swift
+//  ParseChat
+//
+//  Created by Kishor Subedi on 3/3/18.
+//  Copyright © 2018 Kishor Subedi. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class ChatViewController: UIViewController {
+
+    @IBOutlet weak var chatMessageField: UITextField!
+    
+    @IBAction func onSend(_ sender: Any) {
+        let chatMessage = PFObject(className: "Message")
+        chatMessage["text"] = chatMessageField.text ?? ""
+        chatMessage.saveInBackground { (success, error) in
+            if success {
+                print("The message was saved!")
+                self.chatMessageField.text = ""
+            } else if let error = error {
+                print("Problem saving message: \(error.localizedDescription)")
+            }
+        }
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
